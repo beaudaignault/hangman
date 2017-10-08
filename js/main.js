f0d9a4491bb040231e34af3e71e60e78e706c098
Robert Daignault <beau@littlehou.se>
2017-10-08 09:49:26 -0700

    split random word to string

--- js/main.js
+++ js/main.js
@@ -1,40 +1,42 @@
-/*----- constants -----*/
+
+  /*----- constants -----*/
 
 /*----- app's state (variables) -----*/
-var secret, guess, numWrong;
+var secret = [], guess = [], rightGuess = [], numWrong, guessIdx, clickCount = 0
 /*----- cached element references -----*/
-/*----- event listeners -----*/
-/*----- functions -----*/
-
 var wordsArray = [
   "Apples",
   "Bananas",
-  "Pears"
+  "Pears", 
+  "Broom"
 ];
-
+/*----- event listeners -----*/
+/*----- functions -----*/
+$(document).ready(function() {  
+// choose random word from wordsArray, hold it in randomWord
+function randomFromArray() {
 var random = Math.floor(Math.random() * wordsArray.length)
-var randomWord = wordsArray[random];
-
-console.log(randomWord+ " boo")
- 
-//document.body.innerHTML = randomItem;
- 
+var randomToString = wordsArray[random];
+console.log(randomToString);
+secret = randomToString.split("");
+console.log(secret)
+}
+randomFromArray();
 //click on data-char item in page saves data-char in string, and checks against randomWord characters
-$('#keyboard p').click(function(evt) {
-	var charMap = $(this).attr('data-char')
-	console.log(this)
-});
+// if character is in randomWord, create innerHTML element containing the character
 
+ 
 //secret = words[ Math.floor(Math.random()*words.length)];
 //guess = // set to a string of same length as secret, but with placeholders //
 
 
 function render() {
-	renderScore();
-	renderBoard();
-	if (winner) {
-		renderWinner();
-	} else {
-		renderTurn();
-	}
+  renderScore();
+  renderBoard();
+  if (winner) {
+    renderWinner();
+  } else {
+    renderTurn();
+  }
 }
+ });//end ready
