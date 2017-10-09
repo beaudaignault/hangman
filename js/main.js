6829bd48c2440483c76a766de96860129d723fdf
Robert Daignault <beau@littlehou.se>
2017-10-08 21:53:13 -0700

    tidy up

--- js/main.js
+++ js/main.js
@@ -2,12 +2,13 @@
 
 /*----- app's state (variables) -----*/
 var secret = [],
-secretIdx = [],
+  secretIdx = [],
   guess = [],
   rightGuess = [],
-  numWrong = 0, guessIdx, clickCount = 0 
+  numWrong = 0,
+  guessIdx, clickCount = 0
 /*----- cached element references -----*/
- alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var wordsArray = [
   "Apples",
   "Bananas",
@@ -18,68 +19,43 @@ var wordsArray = [
 
 /*----- functions -----*/
 // $(document).ready(function () {
-  // choose random word from wordsArray, hold it in randomWord
-  function randomFromArray() {
-    var random = Math.floor(Math.random() * wordsArray.length)
-    var randomToString = wordsArray[random];
-    console.log(randomToString);
-    secret = randomToString.split("");
-    console.log(secret + ' foo');
+// choose random word from wordsArray, hold it in randomWord
+function randomFromArray() {
+  var random = Math.floor(Math.random() * wordsArray.length)
+  var randomToString = wordsArray[random];
+  console.log(randomToString);
+  secret = randomToString.split("");
+  console.log(secret + ' foo');
 
-    function wordFrontend() {
-          for (var i = 0; i < secret.length; i++) {
-            $('.secretword > ul').append('<li class="conceal" data-idx="'+ (this.secret[i]) + '">' + (this.secret[i]) + '</li>');
-            //let secretIdx = secret.length;
-            //secretIdx  = "_";
-            console.log(this.secret[i] +" voo")
-          };
-        }
-        wordFrontend();
+  function wordFrontend() {
+    for (var i = 0; i < secret.length; i++) {
+      $('.secretword > ul').append('<li class="conceal" data-idx="' + (this.secret[i]) + '"><b>' + (this.secret[i]) + '</b></li>');
+    };
+  
   }
+  wordFrontend();
+
+  }
+  randomFromArray();
   var touchKeys = document.getElementsByTagName("p");
   for (var i = 0, length = touchKeys.length; i < length; i++) {
     var touchKey = touchKeys[i];
-    touchKey.addEventListener('click', (function() {
+    touchKey.addEventListener('click', (function () {
       // `this` refers to the p tag that's been clicked
       // need to keep adding to guess while numWrong < 6
-      if (numWrong < 6){
-        guess.push(this.getAttribute('data-char'));
-        // check char in guess against secret
+      // if (numWrong !== 6) {
+      //   guess.push(this.getAttribute('data-char'));
+      //   // check char in guess against secret
 
-      } else {
-        alert('game over')
-        numWrong++;
-      }
-
-      console.log(this.getAttribute('data-char'));
+      //   console.log(secret + " match");
+      // } else {
+      //   console.log('game over')
+      //   numWrong++;
+      // }
+      console.log(this.getAttribute('data-char')+ " da");
     }), true);
   };
-
-
-
-  // function seeIfMatch(){
-  //     document.getElementsByTagName("p").addEventListener('click', function(evt) {
-  //   $('p').attr('data-char').value ;console.log(evt)
-  //   });
-    
-  //   }
-    // letterChioce = $('b').attr('data-char').value($this)  
-
-  
-//   .data('test', $(this).text());
-// }
-  randomFromArray();
   //click on data-char item in page saves data-char in string, and checks against randomWord characters
-  
-    // var elements = document.getElementById('#success-attempt');
-    // var targetB = elements.getElementsByClassName('conceal')[0];
-    //  targetB.innerText = ' _ ';
-
-    // secretIdx.innerText = " _ ";  
-
- 
-
-  
   // if character is in randomWord, create innerHTML element containing the character
   //secret = words[ Math.floor(Math.random()*words.length)];
   //guess = // set to a string of same length as secret, but with placeholders //
@@ -94,4 +70,4 @@ var wordsArray = [
       renderTurn();
     }
   }
-// }); //end ready
+  // }); //end ready
