3e7a5cb6f7eb70e39e077cb1263aa971f275b6f6
Robert Daignault <beau@littlehou.se>
2017-10-08 18:45:15 -0700

    assign unique attr to element
    Each secret words’ data-char attribute.

--- js/main.js
+++ js/main.js
@@ -2,10 +2,12 @@
 
 /*----- app's state (variables) -----*/
 var secret = [],
+secretIdx = [],
   guess = [],
   rightGuess = [],
-  numWrong, guessIdx, clickCount = 0
+  numWrong = 0, guessIdx, clickCount = 0 
 /*----- cached element references -----*/
+ alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var wordsArray = [
   "Apples",
   "Bananas",
@@ -13,47 +15,83 @@ var wordsArray = [
   "Broom"
 ];
 /*----- event listeners -----*/
+
 /*----- functions -----*/
-$(document).ready(function () {
+// $(document).ready(function () {
   // choose random word from wordsArray, hold it in randomWord
   function randomFromArray() {
     var random = Math.floor(Math.random() * wordsArray.length)
     var randomToString = wordsArray[random];
     console.log(randomToString);
     secret = randomToString.split("");
-    console.log(secret + ' foo')
+    console.log(secret + ' foo');
+
+    function wordFrontend() {
+          for (var i = 0; i < secret.length; i++) {
+            $('.secretword > ul').append('<li class="conceal" data-idx="'+ (this.secret[i]) + '">' + (this.secret[i]) + '</li>');
+            //let secretIdx = secret.length;
+            //secretIdx  = "_";
+            console.log(this.secret[i] +" voo")
+          };
+        }
+        wordFrontend();
   }
+  var touchKeys = document.getElementsByTagName("p");
+  for (var i = 0, length = touchKeys.length; i < length; i++) {
+    var touchKey = touchKeys[i];
+    touchKey.addEventListener('click', (function() {
+      // `this` refers to the p tag that's been clicked
+      // need to keep adding to guess while numWrong < 6
+      if (numWrong < 6){
+        guess.push(this.getAttribute('data-char'));
+        // check char in guess against secret
+
+      } else {
+        alert('game over')
+        numWrong++;
+      }
+
+      console.log(this.getAttribute('data-char'));
+    }), true);
+  };
+
+
+
+  // function seeIfMatch(){
+  //     document.getElementsByTagName("p").addEventListener('click', function(evt) {
+  //   $('p').attr('data-char').value ;console.log(evt)
+  //   });
+    
+  //   }
+    // letterChioce = $('b').attr('data-char').value($this)  
+
+  
+//   .data('test', $(this).text());
+// }
   randomFromArray();
   //click on data-char item in page saves data-char in string, and checks against randomWord characters
-  function wordFrontend() {
-   
-    for (var i = 0; i < secret.length; i++) {
-      let secretIdx = secret.length;
-    
-      $(".secretword > p").append('<b class="conceal" data-idx ='  + secret.length  + '>' + secret[i] + '</b>');
-     
-          var elements = document.getElemntById('success-attempt').getElementsByClassName('conceal');
-          return elements.innerText = ' _ ';
-       
-        // secretIdx.innerText = " _ ";  
-      
-    };
-  }
-  wordFrontend();
+  
+    // var elements = document.getElementById('#success-attempt');
+    // var targetB = elements.getElementsByClassName('conceal')[0];
+    //  targetB.innerText = ' _ ';
+
+    // secretIdx.innerText = " _ ";  
+
+ 
+
+  
   // if character is in randomWord, create innerHTML element containing the character
-
-
   //secret = words[ Math.floor(Math.random()*words.length)];
   //guess = // set to a string of same length as secret, but with placeholders //
 
 
   function render() {
-    renderScore();
-    renderBoard();
+    renderSecret();
+    renderGallows();
     if (winner) {
       renderWinner();
     } else {
       renderTurn();
     }
   }
-}); //end ready
+// }); //end ready
